46d704038fd892333ddffa5692a48060aed1d519
Akshat-1218 <akshatpandey018@gmail.com>
2026-06-23 09:25:47 +0530

    components

---
+++ 02-components/src/components/card.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div>
+      <div className="card">
+              <h1>Akshat Pandey</h1>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati, atque?</p>
+            </div>
+    </div>
+  )
+}
+
+export default Card
