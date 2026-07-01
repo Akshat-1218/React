44e0bab3e230917535c669e5d7c55d14386b2a05
Akshat Pandey <your-email@example.com>
2026-07-01 12:00:14 +0530

    Ui proj

---
+++ 07-ui-project/src/components/Section1/LeftContent.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import 'remixicon/fonts/remixicon.css'
+
+const LeftContent = () => {
+  return (
+    <div className='w-1/3 h-full flex flex-col justify-between' >
+      <div className='p-5'>
+        <h2 className='text-6xl mb-10 font-bold'>Prospective<br/><span> customer</span> segmentation</h2>
+        <p className='text-xl font-medium text-gray-600'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur et eligendi repellat? Mollitia at similique totam corporis recusandae voluptas. Molestias.</p>
+      </div>
+      <div className='text-6xl'>
+        <i className="ri-arrow-right-up-line "></i>
+      </div>
+    </div>
+  )
+}
+
+export default LeftContent
---
+++ 07-ui-project/src/components/Section1/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='flex items-center justify-between px-22 py-8'>
+      <h1 className='bg-black text-white rounded-full px-6 py-2 uppercase'>Target Audience</h1>
+      <button className='bg-gray-200 text-black rounded-full px-6 py-2 uppercase text-sm tracking-widest'>Digital Banking Platform</button>
+    </div>
+  )
+}
+
+export default Navbar
